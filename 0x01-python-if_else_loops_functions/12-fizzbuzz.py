f80af0c0bdc3b233016218bb6bedbcbe81ee8940
Arafa <khalafarafa@gmail.com>
2024-01-04 05:21:33 -0800

    0x01-20

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for x in range (1, 101):
+    for x in range(1, 101):
         if x == 100:
             print("Buzz ", end="")
         elif x % 3 == 0 and x % 5 == 0:
