3517bf3105b85ccda3ec359094cf56795c7230c2
Arafa <khalafarafa@gmail.com>
2024-01-04 05:55:18 -0800

    0x01-21

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * insert_node - function to insert value in order of linked-list
+ * @number: the value to insert
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *node_ptr = *head;
+
+	if(!new_node)
+		return(NULL);
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if(new_node->n < head->n || !head)
+	{
+		new_node->next = node_ptr;
+		return(*head = *new_node);
+	}
+
+	while(node_ptr)
+	{
+		if(new_node->n < node_ptr->next->n || !node_ptr->next)
+		{
+			new_node->next = node_ptr->next;
+			node_ptr->next = new_node;
+			return(node_ptr);
+		}
+		node_ptr = node_ptr->next;
+	}
+	return(NULL);
+	
+}
