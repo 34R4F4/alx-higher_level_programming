1902a7d11907cc4783d6498b6f2bc523f49635cc
Arafa <khalafarafa@gmail.com>
2024-01-04 04:21:56 -0800

    0x01-12

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) in range(97, 123):
+            print("{:c}".format(ord(c - 32)), end="")
+        else:
+            print("{:c}".format(c), end="")
