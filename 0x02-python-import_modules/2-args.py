12469765975b3019bca05442f44aa262f78ee85c
Arafa <khalafarafa@gmail.com>
2024-01-05 14:56:06 -0800

    0x02-07

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argc = len(sys.argv) - 1
+    argc = len(sys.argv) - 1
 
-if argc == 0:
-    print("{} arguments.".format(argc))
-elif argc == 1:
-    print("{} argument:".format(argc))
-else:
-    print("{} arguments:".format(argc))
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
 
-e = 0
-while e < argc:
-    print("{}: {}".format(e + 1, sys.argv[e + 1]))
-    e += 1
+    e = 0
+    while e < argc:
+        print("{}: {}".format(e + 1, sys.argv[e + 1]))
+        e += 1
