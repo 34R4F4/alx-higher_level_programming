e5b37fedbb6af83142082a3068f93220a2b29593
Arafa <khalafarafa@gmail.com>
2024-01-08 15:46:09 -0800

    0x03-12

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list) or (not my_list):
         return None
     list2 = []
     for o in my_list:
