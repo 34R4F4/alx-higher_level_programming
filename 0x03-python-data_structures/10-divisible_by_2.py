59d90f1efe665370e6ce5e3876312b40f757ec34
Arafa <khalafarafa@gmail.com>
2024-01-08 18:33:39 -0800

    0x03-27

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,9 +3,9 @@ def divisible_by_2(my_list=[]):
     if my_list is None:
         return None
     result = []
-    for value in range(len(my_list)):
-        if my_list[value] % 2 == 0:
-            result[value] == True
+    for value in my_list:
+        if value % 2 == 0:
+            result.append(True)
         else:
-            result[value] == False
+            result.append(False)
     return result
