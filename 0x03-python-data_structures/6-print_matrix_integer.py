f485be9fc3792849f3425eeef185e9b5c36874a1
Arafa <khalafarafa@gmail.com>
2024-01-08 18:36:30 -0800

    0x03-29

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
         if len(row) == 0:
             print("")
         for element in range(len(row)):
-            print("{:d}".format(row[element]), 
+            print("{:d}".format(row[element]),
                     end="\n" if element == len(row) - 1 else " ")
