951072f5fceb2332940b5e30fa04729f041f480e
Arafa <khalafarafa@gmail.com>
2024-01-08 18:11:40 -0800

    0x03-22

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     max_v = my_list[0]
-    for vlaue in my_list:
+    for value in range(len(my_list)):
         if my_list[value] > max_v:
             max_v = my_list[value]
     return max_v
