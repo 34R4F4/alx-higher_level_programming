1b577d4903e93786d906b6198382c4076c176bfa
Arafa <khalafarafa@gmail.com>
2024-01-12 08:26:53 -0800

    0x04-01

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sm = map(lambda x : x ** 2, matrix )
+    sm = list(map(lambda row: 
+        list(map(lambda x: x**2, row))
+        , matrix))
     return sm
