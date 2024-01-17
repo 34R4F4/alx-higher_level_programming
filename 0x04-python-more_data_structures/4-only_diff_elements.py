e24f8ce08d856088835234ead9c098d98ce53cfa
Arafa <khalafarafa@gmail.com>
2024-01-17 11:05:04 -0800

    0x04-04-01

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_2:
             common.append(i)
+    for j in set_2:
+        if j not in set_1:
+            common.append(j)
     return common
