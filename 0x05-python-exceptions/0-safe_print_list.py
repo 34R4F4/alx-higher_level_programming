e1204ea38e0cac7704c63cb1f2c381aab3ee1a46
Arafa Khalaf <khalafarafa@gmail.com>
2024-01-22 15:46:39 -0800

    0x05 02

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         while e < x:
             print(my_list[e], end="")
             e = e + 1
-    except:
-        SyntaxError: print("SyntaxError 01")
+    except IndexError:
+        None
     finally:
         print()
         return e
