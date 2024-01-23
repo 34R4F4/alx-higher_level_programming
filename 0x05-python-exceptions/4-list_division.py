f74c8063b9087c076c79333f10cacf16dab4fab8
Arafa Khalaf <khalafarafa@gmail.com>
2024-01-23 00:54:24 -0800

    0x05 11

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    l = 0
     e = 0
     r = []
     while e < list_length:
         try:
             d = my_list_1[e] / my_list_2[e]
             r.append(d)
-            l = l + 1
         except IndexError:
             print("out of range")
             r.append(0)
