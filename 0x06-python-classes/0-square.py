ceb6400ad6e35f66542ab6f5a6764c5b317646f7
Arafa Khalaf <khalafarafa@gmail.com>
2024-01-24 13:36:25 -0800

    0x06 00

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Square Class"""
+
+class Square:
+    """Square Defination"""
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Square Class"""
+
+class Square:
+    
+    def __init__(self, size):
+        """
+        Constructor for Square Class.
+
+        Parameters:
+        - size: An integer representing the size of the square.
+        """
+
+        self.__size = size
+        #Private instance attribute
