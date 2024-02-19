198dabbae7a5dcdc8786c8084d59986e80855333
Arafa Khalaf <khalafarafa@gmail.com>
2024-02-19 00:12:14 -0800

    0x07 3-00

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,8 +6,8 @@ def say_my_name(first_name, last_name=""):
     """Method for printing first name & last name.
 
     Args:
-        first_name: first name (string).
-        last_name: last name (string).
+        first_name (str): first name.
+        last_name (str, optional): last name (default: "").
 
     Raises:
         TypeError: If first_name or last_name are not strings.
@@ -19,8 +19,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     print("My name is {:s} {:s}".format(first_name, last_name))
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
