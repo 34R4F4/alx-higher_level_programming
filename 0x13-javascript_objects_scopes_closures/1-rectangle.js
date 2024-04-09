9811f8bdd2258bb62cd5643ca07b94547fcf0078
Arafa Khalaf <khalafarafa@gmail.com>
2024-04-09 16:53:41 -0700

    0x13 | 01 | 1,2

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor(w, h) {
+    // Initialize the instance attributes
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
