83936c38dfe05a488ae064c75e819b065a76f0a7
Arafa Khalaf <khalafarafa@gmail.com>
2024-04-08 18:10:30 -0700

    0x12-1 01

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* script prints 3 lines:
+ *	“C is fun”
+ *	“Python is cool”
+ *	“JavaScript is amazing”
+ */
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
