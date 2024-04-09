40b6df5781a975602d48b04e64f784fc075ca15e
Arafa Khalaf <khalafarafa@gmail.com>
2024-04-08 20:21:16 -0700

    0x12-2 03

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* script prints a message depending of the number of arguments passed */
+const { argv } = require('node:process');
+const argc = argv.length;
+//console.log(argc);
+if (argc === 2) {
+	console.log("No argument");
+} else if (argc === 3) {
+	console.log("Argument found");
+} else {
+	console.log("Arguments found");
+}
