8a0c495d3068804ab27c803b120f1d370315d5e1
Arafa Khalaf <khalafarafa@gmail.com>
2024-04-09 05:15:17 -0700

    0x12-10 fix 12

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 /* script computes and prints a factorial */
 const x = Number.parseInt(process.argv[2]);
 function factoria (n) {
-  if (n === 0) {
+  if (n === 0 || Number.isNaN(n)) {
 	  return (1);
   } else {
     return (n * factoria(n - 1));
