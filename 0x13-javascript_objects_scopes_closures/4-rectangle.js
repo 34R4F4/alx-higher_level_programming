58a129c4fa3a374103f4c5c25ba67865951c8241
Arafa Khalaf <khalafarafa@gmail.com>
2024-04-09 17:32:31 -0700

    0x13 | 01 | 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+// a class Rectangle that defines a rectangle
+// If w or h is equal to 0 or not a positive integer, create an empty object
+// Create an instance method called print() that prints the rectangle using the character X
+
+class Rectangle {
+  constructor (w, h) {
+    // Initialize the instance attributes
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let c = 0; c < this.height; c++) {
+      let row = '';
+      for (let r = 0; r < this.width; r++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  // instance method called rotate() that exchanges the width and the height of the rectangle
+  rotate() {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  // instance method called double() that multiples the width and the height of the rectangle by 2
+  double() {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+}
+module.exports = Rectangle;
